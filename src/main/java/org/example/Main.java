b8c455d35f7ff9ce45a9c87a4b2c18f1a54114be
gs <gledi1995@gmail.com>
2023-12-26 10:13:41 +0100

    first Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * @author Vorname Nachname (vorname.nachname@dataphone.at)
+ * @since ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        for(int i = 0; i <= 5; i++){
+            System.out.println("i = " + i);
+        }
+    }
+}
