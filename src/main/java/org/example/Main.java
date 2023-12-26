a66237a66d3e47ce06b70a7a03b26c5f3a10771f
gs <gledi1995@gmail.com>
2023-12-26 17:25:54 +0100

    10 Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,14 +21,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*System.out.println("Hello world!");
         System.out.println("Hello world 1!");
         System.out.println("Hello world 2!");
         System.out.println("Hello world 4!");
 
         for(int i = 0; i <= 5; i++){
             System.out.println("i = " + i);
-        }
+        }*/
         try{
             SpringApplication.run(Main.class, args);
         }catch (Exception e){
