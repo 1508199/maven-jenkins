7a5ccf2734964017ba55ebc54b288966321e3e28
gs <gledi1995@gmail.com>
2023-12-26 11:56:10 +0100

    third Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world 1!");
+        System.out.println("Hello world 2!");
 
         for(int i = 0; i <= 5; i++){
             System.out.println("i = " + i);
