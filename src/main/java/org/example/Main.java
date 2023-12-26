f8b730bd21d755efd7601c83e3ee61c37223d95a
gs <gledi1995@gmail.com>
2023-12-26 16:55:32 +0100

    9 Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,25 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Vorname Nachname (vorname.nachname@dataphone.at)
  * @since ${DATE}
  */
+@SpringBootApplication
+@RestController
+@Slf4j
 public class Main {
+
+    @GetMapping("/")
+    public String runImage(){
+        return "docker image created and running ...";
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world 1!");
@@ -13,6 +28,11 @@ public class Main {
 
         for(int i = 0; i <= 5; i++){
             System.out.println("i = " + i);
+        }
+        try{
+            SpringApplication.run(Main.class, args);
+        }catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
